ca0228b7e30d52c11f06305f3dd5057dab491b72
willstrasser <will.strasser@gmail.com>
2015-02-25 22:36:00 -0500

    trying

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 console.log('hellllllooo');
-
-app.set('port', (process.env.PORT || 3000));
-
 var express = require('express')
 var	app = express();
 var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
 
+app.set('port', (process.env.PORT || 3000));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.route('/').get(function(req, res, next){
